66802ef73765ab27086a79ac9aac8f1520e4ffe0
Azat Ayupov <runor1@yandex.ru>
2018-07-22 10:55:29 +0500

    add react-redux

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
 import React, { Component } from 'react'
 import './App.css'
 import List from './components/list/list'
-import { createStore } from 'redux'
-
-const store = createStore(changeCurrent)
-
-store.subscribe(() => {
-  console.log(store.getState());
-})
-
-function changeCurrent(state = {}, action) {
-  if (action.type === 'setCurrent') {
-    if (state.current === action.payload) {
-      return {
-        ...state,
-        current: ''
-      }
-    } else {
-      return {
-        ...state,
-        current: action.payload
-      }
-    }
-  } else return state
-}
-
-window.store = store
 
 class App extends Component {
   state = { current: '' }
-  componentWillMount() {
-    store.subscribe(() => {
-      this.setState(() => {
-        return {
-          ...store.getState(),
-        }
-      })
-    })
-  }
-  currentDispatch = (current) => {
-    store.dispatch({type: 'setCurrent', payload: `${current}`})
-  }
   render() {
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,41 @@ import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-render(<App />, document.getElementById('root'));
+function changeCurrent(state = {}, action) {
+    if (action.type === 'setCurrent') {
+      if (state.current === action.payload) {
+        return {
+          ...state,
+          current: ''
+        }
+      } else {
+        return {
+          ...state,
+          current: action.payload
+        }
+      }
+    } else return state
+  }
+
+const store = createStore(changeCurrent)
+
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+window.store = store
+
+currentDispatch = (current) => {
+    store.dispatch({type: 'setCurrent', payload: `${current}`})
+  }
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
