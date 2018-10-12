0adb42df2f8a1b543435809bbe57064a70159380
Azat Ayupov <runor1@yandex.ru>
2018-10-12 12:01:56 +0500

    add pagination max render

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -4,66 +4,77 @@ import './pagination-btn/pagination-btn'
 import PaginationBtn from './pagination-btn/pagination-btn';
 
 export default class Pagination extends Component {
-    state = {
-        arr: [],
-        show: false
-    }
-    componentWillMount() {
-        this.componentWillReceiveProps(this.props)
-    }
-    componentWillReceiveProps (props) {
-        const { LIST_DB, itemsPerPage } = props
-        const length = LIST_DB / itemsPerPage
-        this.setState(() => {
-            return {
-                arr: []
-            }
+  state = {
+    arr: [],
+    show: false
+  }
+  componentWillMount() {
+    this.componentWillReceiveProps(this.props)
+  }
+  componentWillReceiveProps(props) {
+    const { LIST_DB, itemsPerPage } = props
+    const length = LIST_DB / itemsPerPage
+    this.setState(() => {
+      return {
+        arr: []
+      }
+    })
+    if (length > 1) {
+      this.setState(() => {
+        return {
+          show: true
+        }
+      })
+      for (let i = 0; i < length; i++) {
+        this.setState(state => {
+          return {
+            arr: [...state.arr, i + 1]
+          }
         })
-        if (length > 1) {
-            this.setState(() => {
-                return {
-                    show: true
-                }
-            })
-            for (let i = 0; i < length; i++) {
-                this.setState((state) => {
-                    return {
-                        arr: [...state.arr, i + 1]
-                    }
+      }
+    } else {
+      this.setState(() => {
+        return {
+          show: false
+        }
+      })
+    }
+  }
+  render() {
+    const buttonPrev = this.props.currentPage !== 1 ? <button className='pagination-btn pagination-btn_prev' onClick={this.props.setPrevPage}>{'<'}</button> : null
+    const buttonNext = this.props.LIST_DB / this.props.itemsPerPage > this.props.currentPage ? <button className='pagination-btn pagination-btn_next' onClick={this.props.setNextPage}>{'>'}</button> : null
+    if (this.state.show) {
+      return (
+        <div className="pagination">
+          <div className='pagination__wrapper'>
+            {buttonPrev}
+            <div className='pages'>
+              {
+                this.state.arr.map((item, i, arr) => {
+                  if
+                  (
+                    (Math.abs(this.props.currentPage - item) < 3) ||
+                    (this.props.currentPage < 3 && item < 6) ||
+                    (this.props.currentPage > this.state.arr.length - 2 && item > arr.length - 5)
+                  ) {
+                    return <PaginationBtn
+                      currentPage={item === this.props.currentPage}
+                      num={item}
+                      setCurrentPage={this.props.setCurrentPage}
+                      key={item}
+                    />
+                  }
                 })
-            }
-        } else {
-            this.setState(() => {
-                return {
-                    show: false
-                }
-            })
-        }
+              }
+            </div>
+            {buttonNext}
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div></div>
+      )
     }
-    render() {
-        const buttonPrev = this.props.currentPage !== 1 ? <button className='pagination-btn pagination-btn_prev' onClick={this.props.setPrevPage}>{'<'}</button> : null
-        const buttonNext = this.props.LIST_DB / this.props.itemsPerPage > this.props.currentPage ? <button className='pagination-btn pagination-btn_next' onClick={this.props.setNextPage}>{'>'}</button> : null
-        console.log();
-        if (this.state.show) {
-            return (
-                <div className="pagination">
-                    <div class='pagination__wrapper'>
-                        {buttonPrev}
-                        <div className='pages'>
-                            {
-                                this.state.arr.map((item, i) => {
-                                    return <PaginationBtn currentPage={item === this.props.currentPage} num={item} setCurrentPage={this.props.setCurrentPage} key={item}/>
-                                })
-                            }
-                        </div>
-                        {buttonNext}
-                    </div>
-                </div>
-            );
-        } else {
-            return (
-                <div></div>
-            )
-        }
-    }
+  }
 }
